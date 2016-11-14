fcfdbc3fc37d033c0c2077c4085d1f1377c8f3fa
JuL <jul.funtory@gmail.com>
2016-11-14 11:44:49 +0900

    [bug fix] action mode compatibility

--- app/src/main/java/com/funtory/edgeswipe/sampleapp/SampleActivity2.java
+++ app/src/main/java/com/funtory/edgeswipe/sampleapp/SampleActivity2.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
 import android.view.MotionEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -21,6 +22,8 @@ public class SampleActivity2 extends AppCompatActivity {
     private EdgeSwipeDelegate edgeSwipeDelegate = new EdgeSwipeDelegate();
 
     private static final String EXTRA_URL = "extra_url";
+
+    private ActionMode actionMode;
 
     public static void invoke(Context context, String url){
         Intent i = new Intent(context, SampleActivity2.class);
@@ -55,7 +58,7 @@ public class SampleActivity2 extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                SampleActivity.invoke(view.getContext(), url);    //for test
+                SampleActivity2.invoke(view.getContext(), url);    //for test
                 return true;
             }
 
@@ -69,5 +72,46 @@ public class SampleActivity2 extends AppCompatActivity {
         } else{
             return super.dispatchTouchEvent(ev);
         }
+    }
+
+    /**
+     * Store action mode if it is started
+     * @param mode
+     */
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        super.onActionModeStarted(mode);
+        actionMode = mode;
+    }
+
+    /**
+     * When activity is paused, make sure action mode is ended properly.
+     * This check would feel better to have in onDestroy(), but that seems to be
+     * too late down the life cycle and the crash keeps on occurring. The drawback
+     * of this solution is that the action mode is finished when app is minimized etc.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        endActionMode();
+    }
+
+    /**
+     * Makes sure action mode is ended
+     */
+    private void endActionMode() {
+        if (actionMode != null) {
+            actionMode.finish(); /** immediately calls {@link #onActionModeFinished(ActionMode)} */
+        }
+    }
+
+    /**
+     * Clear action mode every time it finishes.
+     * @param mode
+     */
+    @Override
+    public void onActionModeFinished(ActionMode mode) {
+        super.onActionModeFinished(mode);
+        actionMode = null;
     }
 }
--- edgeswipe/src/main/java/com/funtory/edgeswipe/ESAppCompatActivity.java
+++ edgeswipe/src/main/java/com/funtory/edgeswipe/ESAppCompatActivity.java
@@ -3,6 +3,7 @@ package com.funtory.edgeswipe;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
 import android.view.MotionEvent;
 
 /**
@@ -10,6 +11,7 @@ import android.view.MotionEvent;
  */
 public abstract class ESAppCompatActivity extends AppCompatActivity{
     private EdgeSwipeDelegate edgeSwipeDelegate = new EdgeSwipeDelegate();
+    private ActionMode actionMode;
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
@@ -18,5 +20,46 @@ public abstract class ESAppCompatActivity extends AppCompatActivity{
         } else{
             return super.dispatchTouchEvent(ev);
         }
+    }
+
+    /**
+     * Store action mode if it is started
+     * @param mode
+     */
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        super.onActionModeStarted(mode);
+        actionMode = mode;
+    }
+
+    /**
+     * When activity is paused, make sure action mode is ended properly.
+     * This check would feel better to have in onDestroy(), but that seems to be
+     * too late down the life cycle and the crash keeps on occurring. The drawback
+     * of this solution is that the action mode is finished when app is minimized etc.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        endActionMode();
+    }
+
+    /**
+     * Makes sure action mode is ended
+     */
+    private void endActionMode() {
+        if (actionMode != null) {
+            actionMode.finish(); /** immediately calls {@link #onActionModeFinished(ActionMode)} */
+        }
+    }
+
+    /**
+     * Clear action mode every time it finishes.
+     * @param mode
+     */
+    @Override
+    public void onActionModeFinished(ActionMode mode) {
+        super.onActionModeFinished(mode);
+        actionMode = null;
     }
 }
